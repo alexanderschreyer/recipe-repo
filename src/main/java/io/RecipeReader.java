039ac618df8bd6d2cbdbe55ec56292436aac8cd6
Alexander Schreyer <alexander.schreyer@students.fhnw.ch>
2026-02-12 18:19:50 +0100

    implemented working method to map recipes from JSON file to runtime objects

--- src/main/java/io/RecipeReader.java
+++ src/main/java/io/RecipeReader.java
@@ -1,25 +1,56 @@
 package io;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Recipe;
 
-public class RecipeReader {
+import tools.jackson.databind.ObjectMapper;
+
+public class RecipeRepository {
+    private final String dirPathStr;
+
+    public RecipeRepository(String filePathStr) {
+        this.dirPathStr = filePathStr;
+    }
     
-    public List<Recipe> readRecipesFromJSON(String filePathStr) {
-        String jsonContent;
+    public List<Recipe> readRecipesFromJSON() {
+        List<Recipe> output = new ArrayList<>();
+        Path dirPath = null;
+
+        // Create directory path from directory path string
         try {
-            jsonContent = Files.readString(Path.of(new URI("file:///" + filePathStr)));
-            String[] parts = jsonContent.split("");
-            return null;
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-            return null;
+            dirPath = Path.of(new URI("file:///" + dirPathStr));
+        } catch (URISyntaxException e) {
+            System.err.println("ERROR: Malformed URI");
+            // TODO: LOGGING
         }
+
+        // Iterate over every file in the directory and map to a recipe object
+        if (dirPath != null) {
+            try {
+                List<Path> files = Files.list(dirPath).toList();
+                for (Path file : files) {
+                    InputStream src = Files.newInputStream(file);
+                    ObjectMapper mapper = new ObjectMapper();
+                    Recipe recipe = mapper.readValue(src, Recipe.class);
+                    output.add(recipe);
+                }
+            } catch (IOException e) {
+                System.err.println("ERROR: Files could not be fetched");
+                // TODO: LOGGING
+            }
+        } else {
+            System.err.println("ERROR: dirPath was null");
+            // TODO: LOGGING
+        }
+
+        return output;
     }
 }
