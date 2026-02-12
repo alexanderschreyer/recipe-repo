6f5939a25341f8b1cb6d82dde9019bd2ae9da893
Alexander Schreyer <alexanderschreyer@hotmail.com>
2026-02-12 23:00:45 +0100

    added working implementation for both read and write operations from and to JSON files

--- src/main/java/io/RecipeRepository.java
+++ src/main/java/io/RecipeRepository.java
@@ -1,53 +1,62 @@
 package io;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import model.Recipe;
 
 import tools.jackson.databind.ObjectMapper;
 
 public class RecipeRepository {
-    private final String dirPathStr;
+    private final Path dir = Path.of("data");
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public RecipeRepository(String filePathStr) {
-        this.dirPathStr = filePathStr;
+    public RecipeRepository() {
+        // TODO: READ PATH FROM APPLICATION PROPERTIES (?)
+    }
+
+    private void checkDirectory() {
+        if (!Files.exists(dir)) {
+            try {
+                Files.createDirectory(dir);
+            } catch (IOException e) {
+                // TODO: LOGGING
+                throw new RuntimeException("Data directory could not be created.");
+            }
+        }
+    }
+
+    public void writeRecipeToJSON(Recipe recipe) {
+        checkDirectory();
+        objectMapper.writeValue(new File(dir + "/" + recipe.getId() + ".json"), recipe);
     }
     
     public List<Recipe> readRecipesFromJSON() {
-        List<Recipe> output = new ArrayList<>();
-        Path dirPath = null;
+        checkDirectory();
 
-        // Create directory path from directory path string
-        try {
-            dirPath = Path.of(new URI("file:///" + dirPathStr));
-        } catch (URISyntaxException e) {
-            System.err.println("ERROR: Malformed URI");
-            // TODO: LOGGING
-        }
+        List<Recipe> output = new ArrayList<>();
 
         // Iterate over every file in the directory and map to a recipe object
-        if (dirPath != null) {
-            try {
-                List<Path> files = Files.list(dirPath).toList();
-                for (Path file : files) {
+        if (dir != null) {
+            try (Stream<Path> files = Files.list(dir)) {
+                List<Path> fileList = files.toList();
+                for (Path file : fileList) {
                     InputStream src = Files.newInputStream(file);
-                    ObjectMapper mapper = new ObjectMapper();
-                    Recipe recipe = mapper.readValue(src, Recipe.class);
+                    Recipe recipe = objectMapper.readValue(src, Recipe.class);
                     output.add(recipe);
                 }
             } catch (IOException e) {
-                System.err.println("ERROR: Files could not be fetched");
+                System.err.println("ERROR");
                 // TODO: LOGGING
             }
         } else {
-            System.err.println("ERROR: dirPath was null");
+            System.err.println("ERROR");
             // TODO: LOGGING
         }
 
