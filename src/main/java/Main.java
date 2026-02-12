7f39ef86e4254d1ac2eba7a38a05633fd7721b02
Alexander Schreyer <alexander.schreyer@students.fhnw.ch>
2026-02-12 18:31:44 +0100

    adjusted main class to check reading recipes functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
-import io.JSONParser;
-import io.RecipeReader;
+import io.RecipeRepository;
+import model.Ingredient;
+import model.Recipe;
+
+import java.util.List;
 
 public class Main {
+    public static String dirPathStr = "/Users/alexanderschreyer/Documents/08-personal-projects/05-programming/java/recipe-app/src/main/resources/data/";
 
     public static void main(String[] args) {
-        String filePathStr = "/Users/alexanderschreyer/Documents/08-personal-projects/05-programming/java/recipe-app/src/main/resources/data/example.json";
+        RecipeRepository rr = new RecipeRepository(dirPathStr);
+        List<Recipe> rs = rr.readRecipesFromJSON();
 
-        
+        // Print out recipes to check if read operation was successful
+        for (Recipe r : rs) {
+            System.out.println(r.getName());
+            List<Ingredient> is = r.getIngredients();
+            for (Ingredient i : is) {
+                System.out.println("- " + i.getName() + ", " + i.getQuantity() + " " + i.getUnit());
+            }
+            System.out.println("\n");
+        }
     }
 }
