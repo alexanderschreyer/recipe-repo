88996c36404a3e9aee1aa7f6bc47690131c09ef1
Alexander Schreyer <alexander.schreyer@students.fhnw.ch>
2026-02-13 20:58:38 +0100

    added very basic index page to see if recipe objects are correctly passed to the frontend

--- src/main/java/com/alexanderschreyer/reciperepo/RecipeRepoApplication.java
+++ src/main/java/com/alexanderschreyer/reciperepo/RecipeRepoApplication.java
@@ -3,26 +3,16 @@ package com.alexanderschreyer.reciperepo;
 import com.alexanderschreyer.reciperepo.io.RecipeRepository;
 import com.alexanderschreyer.reciperepo.model.Ingredient;
 import com.alexanderschreyer.reciperepo.model.Recipe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@SpringBootApplication
 public class RecipeRepoApplication {
 
     public static void main(String[] args) {
-        RecipeRepository rr = new RecipeRepository();
-        List<Recipe> rs = rr.readRecipesFromJSON();
-
-        // Print out recipes to check if read operation was successful
-        for (Recipe r : rs) {
-            System.out.println(r.getName());
-            List<Ingredient> is = r.getIngredients();
-            for (Ingredient i : is) {
-                System.out.println("- " + i.getName() + ", " + i.getQuantity() + " " + i.getUnit());
-            }
-            System.out.println("\n");
-        }
-
-        // Recipe recipe = new Recipe("scrambled-eggs", "Scrambled Eggs");
-        // rr.writeRecipeToJSON(recipe);
+        SpringApplication.run(RecipeRepoApplication.class, args);
     }
 }
---
+++ src/main/java/com/alexanderschreyer/reciperepo/controller/RecipeRepoController.java
@@ -0,0 +1,27 @@
+package com.alexanderschreyer.reciperepo.controller;
+
+import java.util.List;
+
+import com.alexanderschreyer.reciperepo.io.RecipeRepository;
+import com.alexanderschreyer.reciperepo.model.Recipe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RecipeRepoController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Recipe> recipes = getRecipes();
+        model.addAttribute("recipes", recipes);
+        return "index";
+    }
+
+    private List<Recipe> getRecipes() {
+        RecipeRepository rr = new RecipeRepository();
+        rr.readRecipesFromJSON();
+        return rr.getRecipes();
+    }
+}
