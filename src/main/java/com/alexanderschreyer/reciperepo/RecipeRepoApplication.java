41d3aeb51e52500da532201ef09c6f3a23cda29c
Alexander Schreyer <alexander.schreyer@students.fhnw.ch>
2026-02-14 10:05:57 +0100

    removed unnecessary imports from main class

--- src/main/java/com/alexanderschreyer/reciperepo/RecipeRepoApplication.java
+++ src/main/java/com/alexanderschreyer/reciperepo/RecipeRepoApplication.java
@@ -1,13 +1,7 @@
 package com.alexanderschreyer.reciperepo;
 
-import com.alexanderschreyer.reciperepo.io.RecipeRepository;
-import com.alexanderschreyer.reciperepo.model.Ingredient;
-import com.alexanderschreyer.reciperepo.model.Recipe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 public class RecipeRepoApplication {
