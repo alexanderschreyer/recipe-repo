8fc5cb42bbb137a845f27f3a712b069d4e4ff816
Alexander Schreyer <alexander.schreyer@students.fhnw.ch>
2026-02-14 10:58:54 +0100

    added functionality to load a specific recipe by clicking on its name on the index page

--- src/main/java/com/alexanderschreyer/reciperepo/controller/RecipeRepoController.java
+++ src/main/java/com/alexanderschreyer/reciperepo/controller/RecipeRepoController.java
@@ -8,19 +8,27 @@ import com.alexanderschreyer.reciperepo.model.Recipe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RecipeRepoController {
+    List<Recipe> recipes;
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Recipe> recipes = getRecipes();
+        RecipeRepository rr = new RecipeRepository();
+        recipes = rr.getRecipes();
         model.addAttribute("recipes", recipes);
         return "index";
     }
 
-    private List<Recipe> getRecipes() {
-        RecipeRepository rr = new RecipeRepository();
-        return rr.getRecipes();
+    @GetMapping("/recipes/{id}")
+    public String recipe(@PathVariable String id, Model model) {
+        for (Recipe recipe : recipes) {
+            if (recipe.getId().equals(id)) {
+                model.addAttribute("recipe", recipe);
+            }
+        }
+        return "recipe";
     }
 }
